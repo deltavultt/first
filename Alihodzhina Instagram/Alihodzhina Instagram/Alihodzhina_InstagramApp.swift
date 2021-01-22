45c5497d12d1aa37e25d2958c693c5298a97f62c
deltavultt <76437365+deltavultt@users.noreply.github.com>
2021-01-22 17:38:45 +0300

    Instagram1

---
+++ Alihodzhina Instagram/Alihodzhina Instagram/Alihodzhina_InstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  Alihodzhina_InstagramApp.swift
+//  Alihodzhina Instagram
+//
+//  Created by WSR on 22.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct Alihodzhina_InstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            RootContentView()
+        }
+    }
+}
